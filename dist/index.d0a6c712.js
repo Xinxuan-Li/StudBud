eb711e92a02f70d1a05fac9d17f7aa7af2d6f938
X Li <83388107+Xinxuan-Li@users.noreply.github.com>
2022-04-27 23:24:16 +1000

    initial commit

--- dist/index.d0a6c712.js
+++ dist/index.d0a6c712.js
@@ -2,8 +2,6 @@ function show() {
     var pomodoro = document.getElementById("pomodoro");
     pomodoro.style.visibility = show;
 }
+function hide() {}
 
-function hide() {
-
-}
-
+//# sourceMappingURL=index.d0a6c712.js.map
